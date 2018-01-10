9cd326ce6a6385b82140a5894bf17f7a51dbb651
David Kim <davidjinuk90@gmail.com>
2018-01-10 00:18:35 -0800

    Created form for blog posts. All posts are now saved in variable allPosts

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.set('view engine', 'ejs');
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let allPosts = [];
+
 app.get('/', function(req, res){
   res.render('index');
 });
@@ -19,6 +21,22 @@ app.get('/posts', function(req, res){
 
 app.get('/posts/submit', function(req, res){
   res.render('submit');
+});
+
+app.post('/posts/submit', function(req, res){
+  addPost();
+
+  function addPost(){
+    let email = req.body.email;
+    let title = req.body.title;
+    let description = req.body.description;
+    let post = {
+      email: email,
+      title: title,
+      description: description
+    };
+    allPosts.push(post);
+  };
 });
 
 app.listen(3000, function(){
